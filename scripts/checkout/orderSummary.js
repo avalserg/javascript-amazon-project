720309aa475ad9d1aa6482843681623b5bf65744
avalserg <avalserg@gmail.com>
2024-08-22 10:10:56 +0300

    Make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,13 +5,7 @@ import {hello  } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { getProduct } from "../../data/products.js";
-
-
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-
-console.log(deliveryDate.format("dddd, MMMM D"));
+import { renderPaymentSummary } from "./paymentSummary.js"; 
 
 export function renderOrderSummary() {
 let cartSummaryHTML = "";
@@ -121,6 +115,7 @@ link.addEventListener("click", () => {
     removeFromCart(productId);
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
+    renderPaymentSummary();
 })
 })
 
@@ -129,6 +124,8 @@ element.addEventListener("click", () => {
     const {productId, deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId, deliveryOptionId);
     renderOrderSummary();
+    renderPaymentSummary();
+
 });
 })
 }
