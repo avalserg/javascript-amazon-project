ff67be694e1d4567b993f6f69e96232851201e6b
avalserg <avalserg@gmail.com>
2024-09-08 09:04:24 +0300

    Learn promices and convert code to promices

--- data/products.js
+++ data/products.js
@@ -97,6 +97,8 @@ export function loadProducts(fun) {
        }
       return new Product(productDetails);
     });
+      console.log('load products');
+      
       fun();
   });
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,54 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 import { loadProducts } from "../data/products.js";
-loadProducts(() => {
-    renderOrderSummary();
+import { loadCart } from "../data/cart.js";
+
+Promise.all([
+new Promise((resolve) => {
+    
+    loadProducts(() => {       
+        resolve('value1');
+    });
+
+}),
+   new Promise((resolve) => {        
+        loadCart(() => {
+            resolve();
+        });
+    }) 
+
+]).then((values) => {
+    console.log(values);
+    
+     renderOrderSummary();
     renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+    
+//     loadProducts(() => {       
+//         resolve('value1');
+//     });
+
+// }).then((value) => {
+
+//     console.log(value);
+    
+//     return new Promise((resolve) => {        
+//         loadCart(() => {
+//             resolve();
+//         });
+//     })
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+
+// loadProducts(() => {
+//     loadCart(() => {
+        
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
